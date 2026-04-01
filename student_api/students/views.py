a7667aff7035135a4b66352b6dd75d1cb55497fa
klae <klaehyunjung@gmail.com>
2026-04-01 21:27:49 +0800

    first commit

---
+++ student_api/students/views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from .models import Student
+from .serializers import StudentSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    lookup_field = 'student_id'  # allows URL to use student_id
